9259fac97185d8ecf1c5c5c83de26c32ca3ae572
jarryp <jarry.palacios@hotmail.com>
2020-09-17 21:34:11 -0600

    Variable de Entorno URL de conexión base de datos mongo

--- server/config/config.js
+++ server/config/config.js
@@ -16,6 +16,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://jarryp:0cdnu3LEff9cTJpI@cluster0.hadys.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
